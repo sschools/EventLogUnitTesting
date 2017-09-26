cd476e6fb142788bc0ebfee817e29af471fcdb45
Stephen Schools <stephen.schools>
2017-09-26 12:00:46 -0400

    set up classes and empty functions

---
+++ src/com/ironyard/EventLog.java
@@ -0,0 +1,20 @@
+package com.ironyard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventLog {
+    private List<Event> eventList = new ArrayList<>();
+
+    public boolean AddEvent(Event event) {
+        return false;
+    }
+
+    public int getNumEvents() {
+        return -2;
+    }
+
+    public EventLog() {
+
+    }
+}
